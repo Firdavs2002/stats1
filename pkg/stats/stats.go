c6445911b8a5e023ef08c279739255f667d8aaaf
Firdavs <FirdavsUrunov>
2020-09-25 03:00:40 +0500

    commited all !

--- pkg/stats/stats.go
+++ pkg/stats/stats.go
@@ -1,18 +1,23 @@
 package stats
 
 import (
-	"github.com/Firdavs2002/bank1/pkg/types"
+	"github.com/Firdavs2002/bank1/v2/pkg/types"
 )
 
 //Avg расчитовает среднюю сумму платежа
 func Avg(payments []types.Payment) types.Money {
-	countPayments := types.Money(len(payments))
+	// countPayments := types.Money(len(payments))
 	sumPaymenys := types.Money(0)
+	indexPayments := types.Money(0)
 	for _, payment := range payments {
+		if payment.Status == types.StatusFail {
+			continue
+		}
 		moneyPayments := payment.Amount
 		sumPaymenys += moneyPayments
+		indexPayments++
 	}
-	return sumPaymenys / countPayments
+	return sumPaymenys / indexPayments
 }
 
 // TotalInCategory находит 	сумму покупок в определённой категории.
@@ -20,6 +25,9 @@ func TotalInCategory(payments []types.Payment, category types.Category) types.Mo
 	sumPaymenys := types.Money(0)
 	for _, payment := range payments {
 		if payment.Category != category {
+			continue
+		}
+		if payment.Status == types.StatusFail {
 			continue
 		}
 		moneyPayments := payment.Amount
